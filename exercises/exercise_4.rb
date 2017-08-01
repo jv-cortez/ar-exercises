4fa6dbe3b254704936a5593caaf1e4ae2f01ee24
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-01 20:16:20 +0000

    added code for exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,19 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+mens_store = Store.where(mens_apparel: true)
+mens_store_with_revenue = mens_store.pluck(:name, :annual_revenue)
+womens_store = Store.where('womens_apparel = ? AND annual_revenue < ?', 'true', 1000000)
+# womens_store_lessthanmil_revenue = womens_store.select { |store| store[:annual_revenue] < 1000000 }
+
+
 puts "Exercise 4"
+puts mens_store_with_revenue
+puts womens_store.inspect
 puts "----------"
 
 # Your code goes here ...
