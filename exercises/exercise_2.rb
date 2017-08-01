1eacbd88e706d76583b89564ca9bf823246644de
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-01 19:24:55 +0000

    added code for exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,5 +1,10 @@
 require_relative '../setup'
 require_relative './exercise_1'
+store1 = Store.find_by(id: 1)
+@store1 = store1
+store2 = Store.find_by(id: 2)
+@store = store2 
+@store1.update(name: 'Surrey')
 
 puts "Exercise 2"
 puts "----------"
