97110903b6af82e6dbaeea86441c92041ad52f46
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-01 20:49:23 +0000

    added new employess and relations within the class files

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,6 +5,16 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nicole", last_name: "Kidman", hourly_rate: 65)
+@store1.employees.create(first_name: "Lucy", last_name: "Liu", hourly_rate: 70)
+
+store2 = Store.find_by(id: 2)
+@store2 = store2
+@store2.employees.create(first_name: "Joel", last_name: "mcJoel", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "mcJohn", hourly_rate: 55)
+@store2.employees.create(first_name: "Nicholas", last_name: "Cage", hourly_rate: 20)
+
 puts "Exercise 6"
 puts "----------"
 
