cdb4bfb9ef5dcb3f57831ea7b5d33f2e15c6ee96
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 00:02:04 +0000

    added 'number of stores label before count and put store count into a variable

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,12 +1,12 @@
 require_relative '../setup'
 
-Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
-Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: false)
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+#Won't add langley, because it did not pass validation. Proves the validation works
+Store.create(name: 'Langley', annual_revenue: 0, mens_apparel: false, womens_apparel: false)
 
-
+count = Store.count
 puts "Exercise 1"
-puts Store.count
 puts "----------"
-
-# Your code goes below here ...
+puts "Number of stores: ", count
