2097b87ee1654255e534ffa4575fe6098933cab8
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 00:02:46 +0000

    moved code block in the appropriate place

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,14 +6,14 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Which store are you looking for?"  
-name = gets.chomp
-puts "Enter store name here: " + name
-store_name = Store.create name: name 
 
-puts store_name.errors.full_messages
 
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Which store are you looking for?"  
+name = gets.chomp
+puts "Store name entered: " + name 
+store = Store.create name: name
+
+puts store.errors.full_messages
