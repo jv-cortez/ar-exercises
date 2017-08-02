20fa71d168fbdc3cd6afa2ed2c78727d2328c06c
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 00:01:23 +0000

    replaced '== false' in the conditional statement in the custom validation with !

--- lib/store.rb
+++ lib/store.rb
@@ -2,5 +2,12 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: {greater_than: 0}
+  validate :must_carry_either_womens_or_mens_apparel
+
+  def must_carry_either_womens_or_mens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:gotta_stock_some_apparel, "They do not carry and men's or women's clothes!")
+    end
+  end
 end
 
